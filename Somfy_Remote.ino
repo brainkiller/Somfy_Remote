15ca47a14a01e39ff3df6af6635cf797f2cba27b
brainkiller <brainkiller@gmail.com>
2018-10-06 21:53:31 +0200

    Added storing of last sent frame (allow repeating frames to be sent with old rolling code)

--- Somfy_Remote.ino
+++ Somfy_Remote.ino
@@ -14,6 +14,7 @@
   To make a group command, just repeat the last two steps with another blind (one by one)
 
   Then:
+    - The array id of the remote address
     - m, u or h will make it to go up
     - s make it stop
     - b, or d will make it to go down
@@ -28,14 +29,21 @@
 #define STOP 0x1
 #define BAS 0x4
 #define PROG 0x8
-#define EEPROM_ADDRESS 0
 
+// Array of IO pins providing power to the RFLink shield
+const int rflink_power_port[] = {15, 16, 17, 18, 20, 21, 22, 23, 46 ,47 ,48 ,49, 50, 51 ,52, 53}; //24, 25, 
+
+// Array of simulated Somfy RTS addresses
 const unsigned long remotes[] = {0x121301, 0x121302, 0x121303, 0x121304};
 
+// Dynamic arrays to store and maintain the rolling codes in the system eeprom
 int eeprom_addresses[sizeof(remotes)] = {0};
 unsigned int rollingCode[sizeof(remotes)]={0};
 
-unsigned int newRollingCode = 101;       //<-- Change it!
+// Initial rolling code for new addresses
+unsigned int newRollingCode = 101;
+
+byte last_frame[sizeof(remotes)][7];
 
 byte frame[7];
 byte checksum;
@@ -46,29 +54,14 @@ void SendCommand(byte *frame, byte sync);
 
 void setup() {
   Serial.begin(115200);
-  
-  pinMode(15, OUTPUT);  digitalWrite(15,HIGH);
-  pinMode(16, OUTPUT);  digitalWrite(16,HIGH);
-  pinMode(17, OUTPUT);  digitalWrite(17,HIGH);
-  pinMode(18, OUTPUT);  digitalWrite(18,HIGH);
-  pinMode(20, OUTPUT);  digitalWrite(20,HIGH);
-  pinMode(21, OUTPUT);  digitalWrite(21,HIGH);
-  pinMode(22, OUTPUT);  digitalWrite(22,HIGH);
-  pinMode(23, OUTPUT);  digitalWrite(23,HIGH);
-  pinMode(24, OUTPUT);  digitalWrite(24,HIGH);
-  pinMode(25, OUTPUT);  digitalWrite(25,HIGH);
-  pinMode(46, OUTPUT);  digitalWrite(46,HIGH);
-  pinMode(47, OUTPUT);  digitalWrite(47,HIGH);
-  pinMode(48, OUTPUT);  digitalWrite(48,HIGH);
-  pinMode(49, OUTPUT);  digitalWrite(49,HIGH);
-  pinMode(50, OUTPUT);  digitalWrite(50,HIGH);
-  pinMode(51, OUTPUT);  digitalWrite(51,HIGH);
-  pinMode(52, OUTPUT);  digitalWrite(52,HIGH);
-  pinMode(53, OUTPUT);  digitalWrite(53,HIGH);
 
-  
-  pinMode(PORT_TX, OUTPUT); // DDRJ |= 1 << PORT_TX; // Pin 5 an output
-  digitalWrite(PORT_TX,LOW); // PORTJ &= !(1 << PORT_TX); // Pin 5 LOW
+  for (int i = 0; i < sizeof(rflink_power_port) / sizeof(rflink_power_port[0]); i++) {
+    pinMode(rflink_power_port[i], OUTPUT);
+    digitalWrite(rflink_power_port[i], HIGH);
+  }
+    
+  pinMode(PORT_TX, OUTPUT);
+  digitalWrite(PORT_TX,LOW);
 
   int eeprom_addresses_size = sizeof(eeprom_addresses[0]);
   
@@ -86,54 +79,82 @@ void setup() {
 }
 
 void loop() {
+  boolean send_frame = false;
+  int remote_id;
+   
   if (Serial.available() > 0) {
-    int r = Serial.parseInt();
-    //int r = 0;
-    Serial.print("Input recieved:"); Serial.println(r);
-    unsigned long remote_address = remotes[r];
-    int eeprom_address = eeprom_addresses[r];
+    remote_id = Serial.parseInt();
+    Serial.print("Input recieved:"); Serial.println(remote_id);
+    unsigned long remote_address = remotes[remote_id];
+    int eeprom_address = eeprom_addresses[remote_id];
     char serie = (char)Serial.read();
-    //char serie = 'p';
     Serial.println("");
     Serial.print("Remote : "); Serial.println(remote_address, HEX);
+    
     if (serie == 'm' || serie == 'u' || serie == 'h') {
       Serial.println("Monte"); // Somfy is a French company, after all.
       BuildFrame(frame, remote_address, eeprom_address, HAUT);
+      send_frame = true;
     }
     else if (serie == 's') {
       Serial.println("Stop");
       BuildFrame(frame, remote_address, eeprom_address, STOP);
+      send_frame = true;
     }
     else if (serie == 'b' || serie == 'd') {
       Serial.println("Descend");
       BuildFrame(frame, remote_address, eeprom_address, BAS);
+      send_frame = true;
     }
     else if (serie == 'p') {
       Serial.println("Prog");
       BuildFrame(frame, remote_address, eeprom_address, PROG);
+      send_frame = true;
+    }
+     else if (serie == 'r') { //repeat last frame
+      Serial.println("Repeating last frame");
+      frame[0] = last_frame[remote_id][0];
+      frame[1] = last_frame[remote_id][1];
+      frame[2] = last_frame[remote_id][2];
+      frame[3] = last_frame[remote_id][3];
+      frame[4] = last_frame[remote_id][4];
+      frame[5] = last_frame[remote_id][5];
+      frame[6] = last_frame[remote_id][6];
+      send_frame = true;
     }
     else {
       Serial.println("Custom code");
       BuildFrame(frame, remote_address, eeprom_address, serie);
+      send_frame = true;
     }
+  }
 
-    Serial.println("");
+  if (send_frame){
     SendCommand(frame, 2);
     for (int i = 0; i < 2; i++) {
       SendCommand(frame, 7);
     }
-  }
-  delay(500);
+
+    last_frame[remote_id][0] = frame[0];
+    last_frame[remote_id][1] = frame[1];
+    last_frame[remote_id][2] = frame[2];
+    last_frame[remote_id][3] = frame[3];
+    last_frame[remote_id][4] = frame[4];
+    last_frame[remote_id][5] = frame[5];
+    last_frame[remote_id][6] = frame[6];
+    
+    delay(500);
+  } 
 }
 
 
 void BuildFrame(byte *frame, unsigned long remote_address, int eeprom_address, byte button) {
   unsigned int code;
   EEPROM.get(eeprom_address, code);
-  frame[0] = 0xA7; // Encryption key. Doesn't matter much
-  frame[1] = button << 4;  // Which button did  you press? The 4 LSB will be the checksum
-  frame[2] = code >> 8;    // Rolling code (big endian)
-  frame[3] = code;         // Rolling code
+  frame[0] = 0xA7;          // Encryption key. Doesn't matter much
+  frame[1] = button << 4;   // Which button did  you press? The 4 LSB will be the checksum
+  frame[2] = code >> 8;     // Rolling code (big endian)
+  frame[3] = code;          // Rolling code
   frame[4] = remote_address >> 16; // Remote address
   frame[5] = remote_address >>  8; // Remote address
   frame[6] = remote_address;       // Remote address
@@ -181,7 +202,8 @@ void BuildFrame(byte *frame, unsigned long remote_address, int eeprom_address, b
   }
   Serial.println("");
   Serial.print("Rolling Code  : "); Serial.println(code);
-  EEPROM.put(eeprom_address, code + 1); //  We store the value of the rolling code in the
+  EEPROM.put(eeprom_address, code + 1);
+  // We store the value of the rolling code in the
   // EEPROM. It should take up to 2 adresses but the
   // Arduino function takes care of it.
 }
@@ -189,45 +211,47 @@ void BuildFrame(byte *frame, unsigned long remote_address, int eeprom_address, b
 void SendCommand(byte *frame, byte sync) {
   if (sync == 2) { // Only with the first frame.
     //Wake-up pulse & Silence
-    digitalWrite(PORT_TX,HIGH); //PORTJ |= 1 << PORT_TX;
+    digitalWrite(PORT_TX,HIGH);
     delayMicroseconds(9415);
-    digitalWrite(PORT_TX,LOW); //PORTJ &= !(1 << PORT_TX);
-    //delayMicroseconds(89565);
+    digitalWrite(PORT_TX,LOW);
+    
     delayMicroseconds(65535);
     delayMicroseconds(24030);
   }
 
   // Hardware sync: two sync for the first frame, seven for the following ones.
   for (int i = 0; i < sync; i++) {
-    digitalWrite(PORT_TX,HIGH); //PORTJ |= 1 << PORT_TX;
+    digitalWrite(PORT_TX,HIGH);
     delayMicroseconds(4 * SYMBOL);
-    digitalWrite(PORT_TX,LOW); //PORTJ &= !(1 << PORT_TX);
+    digitalWrite(PORT_TX,LOW);
     delayMicroseconds(4 * SYMBOL);
   }
 
   // Software sync
-  digitalWrite(PORT_TX,HIGH); //PORTJ |= 1 << PORT_TX;
+  digitalWrite(PORT_TX,HIGH);
   delayMicroseconds(4550);
-  digitalWrite(PORT_TX,LOW); //PORTJ &= !(1 << PORT_TX);
+  digitalWrite(PORT_TX,LOW);
   delayMicroseconds(SYMBOL/2);
 
 
   //Data: bits are sent one by one, starting with the MSB.
   for (byte i = 0; i < 56; i++) {
     if (((frame[i / 8] >> (7 - (i % 8))) & 1) == 1) {
-      digitalWrite(PORT_TX,LOW); //PORTJ &= !(1 << PORT_TX);
+      digitalWrite(PORT_TX,LOW);
       delayMicroseconds(SYMBOL);
-      digitalWrite(PORT_TX,HIGH); //PORTJ ^= 1 << PORT_TX;
+      digitalWrite(PORT_TX,HIGH);
       delayMicroseconds(SYMBOL);
     }
     else {
-      digitalWrite(PORT_TX,HIGH); //PORTJ |= (1 << PORT_TX);
+      digitalWrite(PORT_TX,HIGH);
       delayMicroseconds(SYMBOL);
-      digitalWrite(PORT_TX,LOW); //PORTJ ^= 1 << PORT_TX;
+      digitalWrite(PORT_TX,LOW);
       delayMicroseconds(SYMBOL);
     }
   }
 
-  digitalWrite(PORT_TX,LOW); //PORTJ &= !(1 << PORT_TX);
-  delayMicroseconds(30415); // Inter-frame silence
+  digitalWrite(PORT_TX,LOW);
+
+  // Inter-frame silence
+  delayMicroseconds(30415);
 }
